c1f1d5fa67195c0d0402ddba24c93f1cf55c55ec
Jacopo Mattia Marconi <73340924+JacopoMattiaMarconi@users.noreply.github.com>
2021-03-26 12:51:00 +0100

    Delete Applicazione directory

--- Applicazione/IC13/libs/util.php
+++
@@ -1,8 +0,0 @@
-<?php
-// legge da un array Associativo senza errori, al limite stringa vuota 
-function getArr($A,$index){
-	$ret="";
-   if( isset( $A[$index] ) ) $ret=$A[$index];
-   return $ret;
-}
-?>
