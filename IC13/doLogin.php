b9ded0f50c4b8b27e4adc54dc0f9efc6b6d2f36a
Jacopo Mattia Marconi <73340924+JacopoMattiaMarconi@users.noreply.github.com>
2021-03-26 13:20:14 +0100

    Update doLogin.php

--- IC13/doLogin.php
+++ IC13/doLogin.php
@@ -9,24 +9,19 @@ $pw=getArr($_POST,"password");
 $pw=hash('sha256',$pw);
 print("1");
 try {
-		//prepare query
-		//print($pw);
-	
 		$query = "select * from utenti where username= ? and password= ?";
 		//$query = "hvjfdfbfdbdf";
 		$stmt = $con->prepare( $query );
-		print("2");
-
-
+	
 		$stmt->bindParam(1, $ut);
 		$stmt->bindParam(2, $pw);  
 		//execute our query
-		print("3");
+	
 		$stmt->execute();
 		
 		//store retrieved row to a variable
 		$row = $stmt->fetch(PDO::FETCH_ASSOC);
-		print("4");
+	
 		if ($row){  // password coincide 
 			$user=$row['username'];
 			$mail=$row['mail'];
